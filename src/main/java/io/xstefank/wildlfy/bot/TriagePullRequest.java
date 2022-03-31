f2fd6c24c5db8dcc9daf6ff3641ef5f758022f30
Stanislav Koshchienko <superken126@gmail.com>
2022-03-31 15:40:11 +0200

    Add title check

--- src/main/java/io/xstefank/wildlfy/bot/TriagePullRequest.java
+++ src/main/java/io/xstefank/wildlfy/bot/TriagePullRequest.java
@@ -6,15 +6,19 @@ import io.xstefank.wildlfy.bot.config.WildFlyConfigFile;
 import io.xstefank.wildlfy.bot.config.WildFlyConfigFile.WildFlyRule;
 import io.xstefank.wildlfy.bot.config.util.Matcher;
 import org.jboss.logging.Logger;
+import org.kohsuke.github.GHCommitState;
 import org.kohsuke.github.GHEventPayload;
 import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
 
 import java.io.IOException;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class TriagePullRequest {
-
+    private static final String TOKEN = "Put your token here";
     private static final Logger LOG = Logger.getLogger(TriagePullRequest.class);
 
     void onPullRequestOpened(@PullRequest.Opened GHEventPayload.PullRequest pullRequestPayload,
@@ -42,6 +46,39 @@ public class TriagePullRequest {
             pullRequest.comment("/cc @" + String.join(", @", mentions));
         }
 
+    }
+
+    void onPullRequestEdited(@PullRequest.Edited GHEventPayload.PullRequest pullRequestPayload,
+                             @ConfigFile("wildfly-bot.yml") WildFlyConfigFile wildflyBotConfigFile) throws IOException {
+
+        if (wildflyBotConfigFile == null) {
+            LOG.error("No configuration file available. ");
+            return;
+        }
+
+        GHPullRequest pullRequest = pullRequestPayload.getPullRequest();
+
+        if (Matcher.matches(pullRequest, wildflyBotConfigFile.wildfly.rules.get(0))) {
+            setCommitStatus(pullRequest, GHCommitState.SUCCESS);
+        } else {
+            setCommitStatus(pullRequest, GHCommitState.FAILURE);
+        }
+    }
+
+    private void setCommitStatus(GHPullRequest pullRequest, GHCommitState commitState) throws IOException {
+        String sha = pullRequest.getHead().getSha();
+        String url = pullRequest.getHtmlUrl().toString();
+        String FullName = pullRequest.getRepository().getFullName();
+
+        GitHub gitHub = new GitHubBuilder().withOAuthToken(TOKEN).build();
+
+        gitHub.getRepository(FullName).createCommitStatus(sha, commitState, url, "Title is correct");
+
+        if (commitState.equals(GHCommitState.SUCCESS)) {
+            pullRequest.setBody("- [x] PR title check\n");
+        } else {
+            pullRequest.setBody("- [ ] PR title check\n");
+        }
     }
 }
 
